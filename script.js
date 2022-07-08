ce79fee6dc74e4fc2f003d369a882091bcd5afe4
pradeeps4ini <pradeepsaini@protonmail.com>
2022-07-08 06:13:37 +0530

    hover interactivity on ratings

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 const domElements = (() => {
   const ratingDivs = document.querySelectorAll("input");
+  const cardFront = document.querySelector(".front");
+  const cardBack = document.querySelector(".back");
+  const submitBtn = document.querySelector(".submit-btn");
 
-  return { ratingDivs };
+  return { ratingDivs, cardFront, cardBack, submitBtn };
 })();
 
 
-const getValue = function(e) {
-
-  const value = e.target.value;
-  console.log(value)
+const getValue = function(ratingDivs) {
+  let value;
+  ratingDivs.forEach(div => {
+    (div.checked) ? value = div.value : null;   
+  })
+  return value;
 };
 
-const domInterations = (() => {
-  
-  const { ratingDivs } = { ...domElements };
 
-  ratingDivs.forEach(div => div.addEventListener("click", getValue))
+const changeCheckedInputStyle= function(inputDiv) {
+  inputDiv.classList.toggle("checked");
+};
+
+
+const hideCardFace = function(front, back) {
+  front.classList.toggle("scale-to-zero");
+  back.classList.toggle("scale-to-zero");
+};
+
+
+const displayBack = function(back, value) {
+  back.children[0].children[1].textContent = `You selected ${value} out of 5`;  
+};
+
+
+const domInterations = (() => {
+  const { ratingDivs, cardFront, cardBack, submitBtn } = { ...domElements };
+
+  ratingDivs.forEach(div => {
+    (div.checked) ? changeCheckedInputStyle(div) : null;
+  });
+  
+  submitBtn.addEventListener("click", e => {
+      const rating = getValue(ratingDivs);
+      hideCardFace(cardFront, cardBack);
+      displayBack(cardBack, rating);
+  });
 })();
